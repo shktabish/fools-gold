819d54a3b6beed8b83586652ef20d3c1874b2c56
shktabish <shktab1sh93@gmail.com>
2024-11-30 21:40:47 +0530

    added vertica spacing

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Whitelist from "./components/Whitelist"
 
 const App = () => {
   return (
-	<div className="bg-[#fff6e4] p-2 md:p-4" >
-		{/* <Whitelist /> */}
+	<div className="bg-[#fff6e4] p-2 md:p-4 space-y-4" >
+		<Whitelist />
 		<Signup />
 	</div>
   )
