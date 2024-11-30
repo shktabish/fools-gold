20f82546d436f27d0b9412d8c05a717cf8c67aed
Satyam Vyas <vyas.satyam.04@gmail.com>
2024-11-30 23:06:51 +0530

    added Hero and Telegram components; updated App to include them

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import Signup from "./components/Signup"
-import Whitelist from "./components/Whitelist"
+import Hero from "./components/Hero";
+import Whitelist from "./components/Whitelist";
+import Telegram from "./components/Telegram";
+import Signup from "./components/Signup";
 
 const App = () => {
-  return (
-	<div className="bg-[#fff6e4] p-2 md:p-4 space-y-4" >
-		<Whitelist />
-		<Signup />
-	</div>
-  )
-}
+	return (
+		<div className="bg-[#fff6e4] p-2 md:p-4 space-y-4">
+			<Hero />
+			<Whitelist />
+			<Telegram />
+			<Signup />
+		</div>
+	);
+};
 
-export default App
+export default App;
---
+++ src/components/Telegram.jsx
@@ -0,0 +1,3 @@
+export default function Telegram() {
+	return <div></div>;
+}
