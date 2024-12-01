84cd1d9ae3572064aa63093ba5acc94281e2ddca
Satyam Vyas <vyas.satyam.04@gmail.com>
2024-12-01 16:18:13 +0530

    add Header component and integrate it into App; refactor Footer and Signup components for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import Footer from "./components/Footer"
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Whitelist from "./components/Whitelist";
 import Telegram from "./components/Telegram";
 import Signup from "./components/Signup";
+import Footer from "./components/Footer";
 
 const App = () => {
 	return (
 		<div className="bg-[#fff6e4] p-2 md:p-4 space-y-4">
+			<Header />
 			<Hero />
 			<Whitelist />
 			<Telegram />
 			<Signup />
 			<Footer />
-	</div>
+		</div>
 	);
 };
 
